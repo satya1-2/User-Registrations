cef7e0b7bb136ce8ba04911debb05e63d4a01507
satya <satya143@gmail.com>
2023-03-08 11:38:06 +0530

    Added : Welcome to user registration using junits

---
+++ usingTestCaseUserRegistation/UserRegistrations.java
@@ -0,0 +1,8 @@
+package usingTestCaseUserRegistation;
+
+public class UserRegistrations {
+    public static void main(String[] args) {
+        System.out.println("welcome to user registrations using test case");
+    }
+
+}
